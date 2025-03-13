300275469b9f1a264af43a8be2b66873b2f5217f
Rovic Castro <193394253+roviccastro@users.noreply.github.com>
2025-03-13 20:42:53 +0800

    Feat: Create sort function
    - I had a hard time at first. I was
    tattempting to do it with a case statement
    with <=> but kept encountering a to b and b to a
    problems.

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,7 +1,24 @@
-number_arr = [3, 1, 2, 4]
+require 'pry-byebug'
+number_arr = [50, 34, 23, 54, 12, 9, 4, 1, 60, 0]
 
-def bubble_sort(number_arr)
+def bubble_sort(numbers)
+  bubbled_arr = numbers
   
+  numbers.each_with_index do |n, i|
+    if (i == (numbers.length - 1))
+      bubbled_arr[i] = bubbled_arr[i]
+    elsif numbers[i] > numbers[i + 1]
+      x = numbers[i]
+
+      bubbled_arr[i] = bubbled_arr[i + 1]
+      bubbled_arr[i + 1] = x
+    elsif numbers[i] < numbers[i + 1]
+      bubbled_arr[i] = n
+    end
+  end
+
+  bubble_sort(bubbled_arr) unless numbers == numbers.sort
+  return bubbled_arr if bubbled_arr == numbers.sort
 end
 
-bubble_sort(number_arr)
+p "Sorted: #{bubble_sort(number_arr)}"
